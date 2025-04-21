b45a801ea48bd5581201c9a84471720caba665e7
Sailor Black0 <err0rgast3r2@gmail.com>
2025-04-21 13:42:15 -0600

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '2025-04-21';
+let dataSetVersion = '2025-04-22';
 
 /**
  * Image root, will be appended to the start of every image URL.
